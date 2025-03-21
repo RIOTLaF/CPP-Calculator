7c521ecc4e34f7045e5945384b0d455525546236
RIOT <tavaresnicolas157@gmail.com>
2025-03-21 19:37:07 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+void static print(string text) {
+	cout << text + "\n";
+}
+
+void static pvalue(int value) {
+	cout << value;
+	cout << "\n";
+}
+
+void static calculate() {
+	std::string valor1;
+	std::string valor2;
+	std::string type;
+
+	print("Enter a value");
+	cin >> valor1;
+	print("Enter another value");
+	cin >> valor2;
+	print("Choose + - / or *");
+	cin >> type;
+
+	int newvalue1 = atoi(valor1.c_str());
+	int newvalue2 = atoi(valor2.c_str());
+
+	if (type == "+") {
+		cout << valor1 + " + " + valor2 + " = ";
+		pvalue(newvalue1 + newvalue2);
+	}
+	else if (type == "-") {
+		cout << valor1 + " - " + valor2 + " = ";
+		pvalue(newvalue1 - newvalue2);
+	}
+	else if (type == "/") {
+		cout << valor1 + " / " + valor2 + " = ";
+		pvalue(newvalue1 / newvalue2);
+	}
+	else if (type == "*") {
+		cout << valor1 + " * " + valor2 + " = ";
+		pvalue(newvalue1 * newvalue2);
+	}
+
+	cout << "\n";
+	calculate();
+}
+
+int main() {
+	calculate();
+
+	return 0;
+}
